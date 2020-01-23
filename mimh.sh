f611907219e469563d783183ff483f9bf615cdbd
Emma Highland <ehighland@luc.edu>
2020-01-23 00:23:34 -0600

    Add all files

---
+++ mimh.sh
@@ -0,0 +1,5 @@
+GetOldTweets3 --querysearch "mental illness" --since 2020-01-01 --until 2020-01-22 --maxtweets 3000 --lang en 
+mv output_got.csv mentalillness.csv
+
+GetOldTweets3 --querysearch "mental health" --since 2020-01-01 --until 2020-01-22 --maxtweets 3000 --lang en
+mv output_got.csv mentalhealth.csv
